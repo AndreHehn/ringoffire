b04e1ae677da00283e1f894b986076b9b811380a
Andre Hehn <a.hehn@web.de>
2022-07-05 15:35:04 +0200

    add player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import {MatDialog,} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 @Component({
   selector: 'app-game',
@@ -10,7 +13,7 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string;
   game: Game;
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
@@ -31,4 +34,15 @@ export class GameComponent implements OnInit {
       }, 1000);
     };
   };
+
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name);
+
+    });
+  }
+
 }
