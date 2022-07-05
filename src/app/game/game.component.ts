bb50589f898eb5f81ab86661e8faeaf15b1eb573
Andre Hehn <a.hehn@web.de>
2022-07-05 20:05:14 +0200

    playable but not beautyful

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
-import {MatDialog,} from '@angular/material/dialog';
+import { MatDialog, } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 
@@ -27,6 +27,8 @@ export class GameComponent implements OnInit {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop();
       this.pickCardAnimation = true;
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
 
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
@@ -40,9 +42,9 @@ export class GameComponent implements OnInit {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
     dialogRef.afterClosed().subscribe((name: string) => {
-      this.game.players.push(name);
-
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
     });
   }
-
 }
