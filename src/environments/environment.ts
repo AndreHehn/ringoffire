6675c8fa368e7ce5aec7f73d986480a4c011d3ff
Andre Hehn <a.hehn@web.de>
2022-07-07 22:02:11 +0200

    firebase added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-74a66',
+    appId: '1:380196729435:web:fc9f4f5248556e8bf2857e',
+    storageBucket: 'ring-of-fire-74a66.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyD9p0Lhr2fieFQjErakMkJCGMwMsDoiLjY',
+    authDomain: 'ring-of-fire-74a66.firebaseapp.com',
+    messagingSenderId: '380196729435',
+  },
   production: false
 };
 
