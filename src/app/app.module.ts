3c6926e592ec63367914071884448cdbfcc7e49b
Andre Hehn <a.hehn@web.de>
2022-07-07 22:51:47 +0200

    can load from firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore'; 
+import { AngularFireModule } from '@angular/fire/compat';
+
 
 @NgModule({
   declarations: [
@@ -44,7 +46,8 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog, } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
-
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -13,14 +14,33 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string;
   game: Game;
-  constructor(public dialog: MatDialog) { }
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
+
+    this.route.params.subscribe((params) => {
+      console.log(params['id']);
+
+      this.firestore
+      .collection('games')
+      .doc(params['id'])
+      .valueChanges()
+      .subscribe((game: any) => {
+        console.log('game update', game);
+        this.game.currentPlayer = game.currentPlayer;
+        this.game.stack = game.stack;
+        this.game.players = game.players;
+        this.game.playedCards = game.playedCards;
+      });
+      
+    });
+
   }
 
   newGame() {
     this.game = new Game();
+    this.firestore.collection('games').add(this.game.toJson());
   }
 
   takeCard() {
